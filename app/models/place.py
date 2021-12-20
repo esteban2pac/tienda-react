4c62880b58c647ab6f7d155e46ea3c97302460d4
esteban <estebandeleon8080@gmail.com>
2021-12-20 16:44:47 -0500

    cambios finales

--- app/models/place.py
+++ app/models/place.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from app.database.session import Base
+from app.database.base_class import Base
 
 class Place(Base):
-    __tablename__ = "place"
+    __tablename__ = "places"
     id = Column(Integer,primary_key=True,index=True)
     name = Column(String,index=True)
     description = Column(String)
+    price = Column(Integer)
