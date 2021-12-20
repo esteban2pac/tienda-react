cbd67a18e5cdc2ff290aeec1a5f1f29fa9912727
esteban <estebandeleon8080@gmail.com>
2021-12-20 16:11:00 -0500

    cambios finales

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
- 
+from app.api.api import api_router
+from app.core.config import settings
 app = FastAPI()
 app.add_middleware(
    CORSMiddleware,
@@ -9,7 +10,8 @@ app.add_middleware(
    allow_methods=["*"],
    allow_headers=["*"],
 )
- 
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
 @app.get("/health-check")
 def health_check():
    return "OK"
